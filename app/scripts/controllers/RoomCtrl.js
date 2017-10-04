14e7055c928c3f01440d5ba52fd321c0139735b0
NOVOSIT\kreyes <kreyes616@gmail.com>
2017-10-04 17:08:05 -0400

    done with messages

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,18 +1,25 @@
 (function() {
-  function RoomCtrl(Room, $uibModal) {
+  function RoomCtrl(Room, Message, $uibModal) {
     this.rooms = Room.all;
+    this.messages = Message;
+    this.currentRoom = null;
+
+    this.setActiveRoom = function(room){
+        this.currentRoom = room;
+        this.currentRoom.currentMessages = this.messages.getByRoomId(room.$id);
+  		}
 
     this.addNew = function() {
+      Room.newroomname = this.chatroomname;
       var modalUp = $uibModal.open({
         templateUrl:    '/templates/room-modal.html',
         controller:     'ModalCtrl',
         controllerAs:   '$modal'
       });
     }
-
   }
 
   angular
   .module('blocChat')
-  .controller('RoomCtrl', ['Room', '$uibModal', RoomCtrl]);
+  .controller('RoomCtrl', ['Room', 'Message', '$uibModal', RoomCtrl]);
 })();
